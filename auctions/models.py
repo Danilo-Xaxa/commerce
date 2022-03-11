2b68664ded6805c8714353910da2b5ed849aec5b
Danilo Xaxa <daniloxaxa01@gmail.com>
2022-03-11 20:38:35 -0300

    voltando pro models.py original

--- auctions/models.py
+++ auctions/models.py
@@ -4,30 +4,3 @@ from django.db import models
 
 class User(AbstractUser):
     pass
-
-
-class Bid(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    auction = models.ForeignKey("Auction", on_delete=models.CASCADE)
-    amount = models.IntegerField()
-
-
-class Auction(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    highest_bid = models.IntegerField(default=0)
-    highest_bidder = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
-    text = models.TextField()
-
-    def __str__(self):
-        return self.text
